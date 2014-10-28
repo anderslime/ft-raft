bef56214f2bf133841cbabd0f5f8f585dd1561c8
anderslime <aemilnielsen@gmail.com>
2014-10-28 12:17:31 +0100

    Server file with some mocha/chai tests

--- raft/server.js
+++ raft/server.js
@@ -1,9 +1,16 @@
-var raft = {};
-
 Server = (function() {
-  function Server(index, peers) {
+  function Server(index, peers, state) {
     this.index = index;
     this.peers = peers;
+    this.state = state || 'follower';
+  };
+
+  Server.prototype.becomeLeader = function() {
+    this.state = 'leader';
+  };
+
+  Server.prototype.startElection = function() {
+    this.state = 'candidate';
   };
 
   return Server;
--- test/server_test.js
+++ test/server_test.js
@@ -2,9 +2,24 @@ var assert = require('chai').assert;
 var Server = require('../raft/server');
 
 describe("Server", function() {
-  it("is created with index and peers", function() {
-    var server = new Server(1, []);
+  it("is created with paramters", function() {
+    var server = new Server(1, [], 'follower');
     assert.equal(server.index, 1);
     assert.sameMembers(server.peers, []);
+    assert.equal(server.state, 'follower');
+  });
+
+  it("becomes leader", function() {
+    var server = new Server(1, [], 'follower');
+    server.becomeLeader();
+    assert.equal(server.state, 'leader');
+  });
+
+  context("when starting an election", function() {
+    it("becomes a candidate", function() {
+      var server = new Server(1, [], 'follower');
+      server.startElection();
+      assert.equal(server.state, 'candidate');
+    });
   });
 });
