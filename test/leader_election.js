b7ee82a62911443a90257fe717fd1da3ce5d54ea
anderslime <aemilnielsen@gmail.com>
2014-11-11 09:24:42 +0100

    Restructure tests to look like the raft specification

--- test/leader_election.js
+++ test/leader_election.js
@@ -39,78 +39,74 @@ describe("Leader election", function() {
 
   });
 
-  describe("server sends RequestVote to all other servers", function() {
-    it("all other servers sets votedFor to candidate", function() {
-      var server1 = new Server(1, [], 'follower');
-      var server2 = new Server(2, [], 'follower');
-      var server3 = new Server(3, [], 'follower');
-      updatePeers([server1, server2, server3])
-      server1.onTimeout()
-      assert.equal(server2.votedFor, 1)
-      assert.equal(server3.votedFor, 1)
-    });
-
-    it("all other servers updates their term", function() {
-      var server1 = new Server(1, [], 'follower');
-      var server2 = new Server(2, [], 'follower');
-      var server3 = new Server(3, [], 'follower');
-      updatePeers([server1, server2, server3])
-      server1.onTimeout()
-      assert.equal(server2.currentTerm, server1.currentTerm)
-      assert.equal(server3.currentTerm, server1.currentTerm)
-    });
-  });
-
-  describe("Requesting vote on election", function() {
-    // Rules for Servers: 1. and 2.
-    it("is granted a vote", function() {
-      var server1 = new Server(1, [], 'candidate', 1);
-      var server2 = new Server(2, [], 'follower', 0);
-      var voteResponse = server1.invokeVoteRequest(server2);
-      assert.equal(voteResponse.term, 1);
-      assert.equal(voteResponse.voteGranted, true);
-      assert.equal(server2.votedFor, 1);
-    });
-
-    // RequestVote RPC: 1. Reply false if term < currentTerm
-    it("is not granted a vote if vote term is less than target server's term", function() {
-      var server1 = new Server(1, [], 'candidate', 1);
-      var server2 = new Server(2, [], 'follower', 2);
-      var voteResponse = server1.invokeVoteRequest(server2);
-      assert.equal(voteResponse.term, 2);
-      assert.equal(voteResponse.voteGranted, false);
-    });
-
-    // Rules for Servers: 2. If RPC request or response contains term T > currentTerm:
-    // set currentTerm = T, convert to follower
-    it("is converted to follower and updates term when outdated", function() {
-      var server1 = new Server(1, [], 'candidate', 1);
-      var server2 = new Server(2, [], 'follower', 2);
-      var voteResponse = server1.invokeVoteRequest(server2);
-      assert.equal(voteResponse.voteGranted, false);
-      assert.equal(server1.state, 'follower');
+  // Rules for servers: Candidates ($5.2)
+  describe("On conversion to candidate, start election:", function() {
+    it("increments currentTerm", function() {
+      var server1 = new Server(1, [], 'follower', 1);
+      server1.onTimeout();
       assert.equal(server1.currentTerm, 2)
     });
 
-    // Rules for Servers: 2. If RPC request or response contains term T > currentTerm:
-    // set currentTerm = T, convert to follower
-    it("is not when target server has already voted for another server", function() {
-      var server1 = new Server(1, [], 'candidate', 1);
-      var server2 = new Server(2, [], 'follower', 1);
-      server2.votedFor = 3;
-      var voteResponse = server1.invokeVoteRequest(server2);
-      assert.equal(voteResponse.voteGranted, false);
+    it("votes for self", function() {
+      var server1 = new Server(1, [], 'follower', 1);
+      server1.onTimeout();
+      assert.equal(server1.votedFor, 1)
     });
 
-    // Rules for Servers: 2. If RPC request or response contains term T > currentTerm:
-    // set currentTerm = T, convert to follower
-    it("is not granted a vote when the candidate log is not up to date", function() {
-      var server1 = new Server(1, [], 'candidate', 2);
-      server1.log = [{"index": 1, "term": 1}];
-      var server2 = new Server(2, [], 'follower', 2);
-      server2.log = [{"index": 1, "term": 1}, {"index": 5, "term": 2}];
-      var voteResponse = server1.invokeVoteRequest(server2);
-      assert.equal(voteResponse.voteGranted, false);  
+    it("resets election timer", function() {
+      // TODO: Test this
+    });
+
+    describe("sending RequestVote RPCs to all other servers", function() {
+      it("is granted a vote", function() {
+        var server1 = new Server(1, [], 'candidate', 1);
+        var server2 = new Server(2, [], 'follower', 0);
+        var voteResponse = server1.invokeVoteRequest(server2);
+        assert.equal(voteResponse.term, 1);
+        assert.equal(voteResponse.voteGranted, true);
+        assert.equal(server2.votedFor, 1);
+      });
+
+      // RequestVote RPC: 1. Reply false if term < currentTerm
+      it("is not granted a vote if vote term is less than target server's term", function() {
+        var server1 = new Server(1, [], 'candidate', 1);
+        var server2 = new Server(2, [], 'follower', 2);
+        var voteResponse = server1.invokeVoteRequest(server2);
+        assert.equal(voteResponse.term, 2);
+        assert.equal(voteResponse.voteGranted, false);
+      });
+
+      // Rules for Servers: 2. If RPC request or response contains term T > currentTerm:
+      // set currentTerm = T, convert to follower
+      it("is converted to follower and updates term when outdated", function() {
+        var server1 = new Server(1, [], 'candidate', 1);
+        var server2 = new Server(2, [], 'follower', 2);
+        var voteResponse = server1.invokeVoteRequest(server2);
+        assert.equal(voteResponse.voteGranted, false);
+        assert.equal(server1.state, 'follower');
+        assert.equal(server1.currentTerm, 2)
+      });
+
+      // Rules for Servers: 2. If RPC request or response contains term T > currentTerm:
+      // set currentTerm = T, convert to follower
+      it("is not granted a vote when target server that has already voted for another server", function() {
+        var server1 = new Server(1, [], 'candidate', 1);
+        var server2 = new Server(2, [], 'follower', 1);
+        server2.votedFor = 3;
+        var voteResponse = server1.invokeVoteRequest(server2);
+        assert.equal(voteResponse.voteGranted, false);
+      });
+
+      // Rules for Servers: 2. If RPC request or response contains term T > currentTerm:
+      // set currentTerm = T, convert to follower
+      it("is not granted a vote when the candidate log is not up to date", function() {
+        var server1 = new Server(1, [], 'candidate', 2);
+        server1.log = [{"index": 1, "term": 1}];
+        var server2 = new Server(2, [], 'follower', 2);
+        server2.log = [{"index": 1, "term": 1}, {"index": 5, "term": 2}];
+        var voteResponse = server1.invokeVoteRequest(server2);
+        assert.equal(voteResponse.voteGranted, false);
+      });
     });
   });
 
@@ -123,17 +119,6 @@ describe("Leader election", function() {
       updatePeers([server1, server2, server3])
       server1.onTimeout()
       assert.equal(server1.state, 'leader');
-    });
-
-    // Rules for Servers: Candidates: If votes received from majority of servers: become leader
-    it("becomes a leader when it has received the majority of votes", function() {
-      var server1 = new Server(1, [], 'follower');
-      var server2 = new Server(2, [], 'follower');
-      var server3 = new Server(3, [], 'follower');
-      updatePeers([server1, server2, server3])
-      server3.onTimeout()
-      server1.onTimeout()
-      assert.equal(server3.state, 'leader');
     });
   });
 });
