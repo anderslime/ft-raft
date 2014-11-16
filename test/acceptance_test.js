a0af6ede580dec35ca16c1168d861f57f9906efa
anderslime <aemilnielsen@gmail.com>
2014-11-16 17:41:56 +0100

    Move client log entry tests to acceptance tests

--- test/acceptance_test.js
+++ test/acceptance_test.js
@@ -245,3 +245,27 @@ describe("Rules for Candidates", function() {
     });
   });
 });
+
+describe("Client Log Entry Request", function() {
+  it("client sends log request to server", function() {
+    var server = new Server(1, [], 'leader');
+    var logEntry = {"index": 1};
+    var response = server.onReceiveRequest(logEntry);
+    assert.equal(server.log.length, 1);
+    assert.equal(server.lastLogEntry().index, 1);
+    assert.equal(server.lastLogEntry().term, 0);
+    assert.equal(response.isSuccessful, true);
+
+  });
+
+  it("client sends log request to a follower", function() {
+    var server1 = new Server(1, [], 'follower');
+    var server2 = new Server(2, [], 'leader');
+    server1.addPeer(server2);
+    server2.addPeer(server1);
+    var response = server1.onReceiveRequest(1);
+    assert.equal(response.isSuccessful, false);
+    assert.equal(response.leaderId, 2);
+    assert.equal(server1.log.length, 0);
+  });
+});
