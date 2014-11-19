d2f292d997eb66228283051707f16ed6de0d26a5
anderslime <aemilnielsen@gmail.com>
2014-11-19 13:09:05 +0100

    Spaces instead of tabs

--- raft/leader_state.js
+++ raft/leader_state.js
@@ -1,33 +1,33 @@
 LeaderState = ( function(){
-	function LeaderState(lastLogIndex){
-		this.lastLogIndex = lastLogIndex;
-		this.nextIndex = [];
-		this.matchIndex = [];
-	};
+  function LeaderState(lastLogIndex){
+    this.lastLogIndex = lastLogIndex;
+    this.nextIndex = [];
+    this.matchIndex = [];
+  };
 
-	LeaderState.prototype.nextIndexFor = function(peerIndex) {
-		if (this.nextIndex[peerIndex] === undefined) return this.lastLogIndex + 1;
-		return this.nextIndex[peerIndex];
-	};
+  LeaderState.prototype.nextIndexFor = function(peerIndex) {
+    if (this.nextIndex[peerIndex] === undefined) return this.lastLogIndex + 1;
+    return this.nextIndex[peerIndex];
+  };
 
-	LeaderState.prototype.decrementNextIndex = function(peerIndex) {
-		this.nextIndex[peerIndex] = this.nextIndexFor(peerIndex) - 1;
-	};
+  LeaderState.prototype.decrementNextIndex = function(peerIndex) {
+    this.nextIndex[peerIndex] = this.nextIndexFor(peerIndex) - 1;
+  };
 
-	LeaderState.prototype.incrementNextIndex = function(peerIndex) {
-		this.nextIndex[peerIndex] = this.nextIndexFor(peerIndex) + 1;
-	};
+  LeaderState.prototype.incrementNextIndex = function(peerIndex) {
+    this.nextIndex[peerIndex] = this.nextIndexFor(peerIndex) + 1;
+  };
 
-	LeaderState.prototype.setMatchIndex = function(peerIndex, matchedIndex) {
-		this.matchIndex[peerIndex] = matchedIndex;
-	};
+  LeaderState.prototype.setMatchIndex = function(peerIndex, matchedIndex) {
+    this.matchIndex[peerIndex] = matchedIndex;
+  };
 
-	LeaderState.prototype.matchIndexFor = function(peerIndex) {
-		return this.matchIndex[peerIndex];
-	};
+  LeaderState.prototype.matchIndexFor = function(peerIndex) {
+    return this.matchIndex[peerIndex];
+  };
 
 
-	return LeaderState;
+  return LeaderState;
 
 })();
 
