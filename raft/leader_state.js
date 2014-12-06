8a24b1f94fe51cd55d685c54d1a84284a4c8744f
anderslime <aemilnielsen@gmail.com>
2014-12-06 20:57:21 +0100

    Documentation for LeaderState

--- raft/leader_state.js
+++ raft/leader_state.js
@@ -1,33 +1,57 @@
 LeaderState = ( function(){
+  /**
+   * Extraction of state that is unique to the leader Server.
+   * @constructor
+   * @param {Array} otherPeers - List of other Server objects in the cluster.
+   * @param {number} lastLogIndex - The last log index when the server became leader.
+   */
   function LeaderState(otherPeers, lastLogIndex){
     this.lastLogIndex = lastLogIndex;
     this.nextIndex = this._initialNextIndex(otherPeers, lastLogIndex);
     this.matchIndex = this._initialMatchIndex(otherPeers);
   };
 
-  LeaderState.prototype.nextIndexFor = function(peerIndex) {
-    if (this.nextIndex[peerIndex] === undefined) return this.lastLogIndex + 1;
-    return this.nextIndex[peerIndex];
+  /**
+  * @returns the nextIndexFor the peer with the given id
+  * @param {number} peerId - The id of the peer to return next index for.
+  */
+  LeaderState.prototype.nextIndexFor = function(peerId) {
+    if (this.nextIndex[peerId] === undefined) return this.lastLogIndex + 1;
+    return this.nextIndex[peerId];
   };
 
-  LeaderState.prototype.decrementNextIndex = function(peerIndex) {
-    this.nextIndex[peerIndex] = this.nextIndexFor(peerIndex) - 1;
+  /**
+  * Decrements the next index for the peer with the given peerId
+  * @param {number} peerId - The id of the peer to decrement next index for.
+  */
+  LeaderState.prototype.decrementNextIndex = function(peerId) {
+    this.nextIndex[peerId] = this.nextIndexFor(peerId) - 1;
   };
 
-  LeaderState.prototype.incrementNextIndex = function(peerIndex) {
-    this.nextIndex[peerIndex] = this.nextIndexFor(peerIndex) + 1;
+  /**
+  * Set next log index for the given peer
+  * @param {number} peerId - The id of the peer to set next index for.
+  * @param {number} nextIndex - The index to set as next log index for the given peer.
+  */
+  LeaderState.prototype.setNextIndex = function(peerId, nextIndex) {
+    this.nextIndex[peerId] = nextIndex;
   };
 
-  LeaderState.prototype.setNextIndex = function(peerIndex, nextIndex) {
-    this.nextIndex[peerIndex] = nextIndex;
+  /**
+  * Set match index for the given peer.
+  * @param {number} peerId - The id of the peer to set match index for.
+  * @param {number} matchedIndex - The index to set as matching index for the given peer.
+  */
+  LeaderState.prototype.setMatchIndex = function(peerId, matchedIndex) {
+    this.matchIndex[peerId] = matchedIndex;
   };
 
-  LeaderState.prototype.setMatchIndex = function(peerIndex, matchedIndex) {
-    this.matchIndex[peerIndex] = matchedIndex;
-  };
-
-  LeaderState.prototype.matchIndexFor = function(peerIndex) {
-    var matchIndex = this.matchIndex[peerIndex];
+  /**
+  * @returns the matching index for given peer.
+  * @param {number} peerId - The id of the peer to return match index for.
+  */
+  LeaderState.prototype.matchIndexFor = function(peerId) {
+    var matchIndex = this.matchIndex[peerId];
     if (matchIndex === undefined) return -1;
     return matchIndex;
   };
