a6ca574e9bb6e120202099edbdeec0717b6f2269
anderslime <aemilnielsen@gmail.com>
2014-12-06 21:05:18 +0100

    No closure for Direct protocol

--- raft/protocol/direct.js
+++ raft/protocol/direct.js
@@ -1,29 +1,25 @@
-Direct = (function() {
-  function Direct(cluster) {
-    this.cluster = cluster;
-  };
+function Direct(cluster) {
+  this.cluster = cluster;
+};
 
-  Direct.prototype.invokeVoteRequest = function(sourcePeerId, targetPeerId, requestVote) {
-    var targetPeer = this.cluster.findPeer(targetPeerId);
-    return targetPeer.onReceiveRequestVote(sourcePeerId, requestVote);
-  };
+Direct.prototype.invokeVoteRequest = function(sourcePeerId, targetPeerId, requestVote) {
+  var targetPeer = this.cluster.findPeer(targetPeerId);
+  return targetPeer.onReceiveRequestVote(sourcePeerId, requestVote);
+};
 
-  Direct.prototype.invokeVoteResponse = function(sourcePeerId, voteResponse) {
-    var sourcePeer = this.cluster.findPeer(sourcePeerId);
-    return sourcePeer.invokeVoteResponse(voteResponse);
-  };
+Direct.prototype.invokeVoteResponse = function(sourcePeerId, voteResponse) {
+  var sourcePeer = this.cluster.findPeer(sourcePeerId);
+  return sourcePeer.invokeVoteResponse(voteResponse);
+};
 
-  Direct.prototype.invokeAppendEntries = function(sourcePeerId, targetPeerId, appendEntry) {
-    var targetPeer = this.cluster.findPeer(targetPeerId);
-    return targetPeer.onReceiveAppendEntries(sourcePeerId, appendEntry);
-  };
+Direct.prototype.invokeAppendEntries = function(sourcePeerId, targetPeerId, appendEntry) {
+  var targetPeer = this.cluster.findPeer(targetPeerId);
+  return targetPeer.onReceiveAppendEntries(sourcePeerId, appendEntry);
+};
 
-  Direct.prototype.invokeAppendEntriesResponse = function(sourcePeerId, targetPeerId, appendEntryResponse) {
-    var sourcePeer = this.cluster.findPeer(sourcePeerId);
-    return sourcePeer.invokeAppendEntriesResponse(targetPeerId, appendEntryResponse)
-  };
-
-  return Direct;
-})();
+Direct.prototype.invokeAppendEntriesResponse = function(sourcePeerId, targetPeerId, appendEntryResponse) {
+  var sourcePeer = this.cluster.findPeer(sourcePeerId);
+  return sourcePeer.invokeAppendEntriesResponse(targetPeerId, appendEntryResponse)
+};
 
 module.exports = Direct;
