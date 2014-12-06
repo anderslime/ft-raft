c41b7e939031c49bb31c4e34e86a6cd204a0307d
anderslime <aemilnielsen@gmail.com>
2014-12-06 20:48:05 +0100

    Documentation and make private methods private for Cluster

--- raft/cluster.js
+++ raft/cluster.js
@@ -1,17 +1,32 @@
 Cluster = (function() {
+  /**
+   * The Cluster is responsible for the network of peers.
+   * @constructor
+   * @param {Array} peers - List of Server objects with other peers.
+   */
   function Cluster(peers) {
     this.peers = peers || [];
   };
 
+  /**
+  * Adds a peer to the server list of other peers in the Raft cluster.
+  * @param {Server} server - The Server to be added as peer
+  */
   Cluster.prototype.addPeer = function(server){
     this.peers.push(server);
   };
 
+  /**
+  * @returns the id of the leader of the Raft cluster.
+  */
   Cluster.prototype.leaderId = function() {
     var leader = this.leader();
     return leader && leader.id;
   };
 
+  /**
+  * @returns the leader of the Raft cluster.
+  */
   Cluster.prototype.leader = function() {
     var leader = {};
     for (peerIndex in this.peers) {
@@ -23,6 +38,10 @@ Cluster = (function() {
     return leader;
   };
 
+  /**
+  * @param {number} peerId - The id of the peer to find.
+  * @returns the Server object of the peer with the given peerId.
+  */
   Cluster.prototype.findPeer = function(peerId) {
     var peer = {};
     for (var peerIndex in this.peers) {
@@ -34,24 +53,39 @@ Cluster = (function() {
     return peer;
   };
 
+  /**
+  * Can tell if a given number is larger than or equal to the majority number
+  * of servers in the cluster.
+  *
+  * Example:
+  * If there are 5 servers in the cluster
+  * isLargerThanMajority(4) == true
+  * isLargerThanMajority(3) == true
+  * isLargerThanMajority(2) == false
+  * @returns true if the given number 'amount' is larger or equal than the
+  * majority size of servers in the cluster, else false.
+  */
   Cluster.prototype.isLargerThanMajority = function(amount) {
-    return amount >= this.peerMajoritySize();
+    return amount >= this._peerMajoritySize();
   };
 
-  Cluster.prototype.peerMajoritySize = function() {
-    if (this.hasEventNumberOfPeers()) {
+  /**
+  * @returns the amount of peers in the Raft cluster.
+  */
+  Cluster.prototype.amountOfPeers = function() {
+    return this.peers.length;
+  };
+
+  Cluster.prototype._peerMajoritySize = function() {
+    if (this._hasEventNumberOfPeers()) {
       return this.amountOfPeers() / 2 + 1;
     } else {
       return Math.ceil(this.amountOfPeers() / 2);
     }
   };
 
-  Cluster.prototype.hasEventNumberOfPeers = function() {
+  Cluster.prototype._hasEventNumberOfPeers = function() {
     return this.amountOfPeers() % 2 == 0;
-  };
-
-  Cluster.prototype.amountOfPeers = function() {
-    return this.peers.length;
   };
 
   return Cluster;
